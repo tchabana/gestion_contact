5c41288b66a979b5f89e285cada942503f38b239
tchabana <hafiztchabana2004@gmail.com>
2023-11-08 11:48:32 +0000

    ajout de commentaire

--- contacts/models.py
+++ contacts/models.py
@@ -17,13 +17,16 @@ class Contact(models.Model):
         return f"{self.first_name} {self.last_name} {self.phone}"
 
     def save(self,*args, **kwargs):
-        qr_image = qrcode.make(f"Nom: {self.first_name} \n PreNom: {self.last_name} \n Tel: {self.phone}")
-        canvas = Image.new('RGB',(400,400),'white')
-        drow = ImageDraw.Draw(canvas)
-        canvas.paste(qr_image)
-        file_name = f"qr_{self.pk}.png"
-        buffer = BytesIO()
+        qr_image = qrcode.make(f"Nom: {self.first_name} \n PreNom: {self.last_name} \n Tel: {self.phone}")  # Générer le code QR en utilisant les informations du contact
+        canvas = Image.new('RGB',(400,400),'white')     # Créer une image blanche de 400x400 pixels
+        
+        drow = ImageDraw.Draw(canvas)           # Créer un objet "drow" pour dessiner sur le canevas
+        canvas.paste(qr_image)                  # Coller l'image du code QR sur le canevas blanc
+        file_name = f"qr_{self.pk}.png"         # Générer un nom de fichier unique pour l'image du code QR
+        
+        buffer = BytesIO()                      # Créer un tampon de mémoire (buffer) pour enregistrer l'image en format PNG
         canvas.save(buffer,'png')
-        self.qr_code_image.save(file_name,File(buffer),save=False)
-        canvas.close
-        return super().save(*args, **kwargs)
+        
+        self.qr_code_image.save(file_name,File(buffer),save=False) # Enregistrer l'image dans le champ qr_code_image
+        canvas.close  # Fermer le canevas
+        return super().save(*args, **kwargs) # Appeler la méthode save() de la classe parent (super) pour effectuer la sauvegarde en base de données
